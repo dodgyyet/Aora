443ce724398beecdee8320bbfcabcc02cb32524f
Joe Roche <joeroche@Joes-MacBook-Air.local>
2025-03-14 08:37:12 -0400

    Initial Commit
    Basic onboarding screen and layout of app

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,17 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const SignIn = () => {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h[85vh] px-4 my-6">
+          <Text>rat</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
